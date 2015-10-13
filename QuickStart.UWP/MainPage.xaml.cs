b50cea87bda2b37944af9c238c3d2f341a672108
Adrian Hall <adrian@shellmonger.com>
2015-10-12 19:53:44 -0700

    Initial Checkin of layout

---
+++ QuickStart.UWP/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace QuickStart.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e) {
+          var state = "Normal";
+            if (e.NewSize.Width < 600 || e.NewSize.Height < 600)
+            {
+                state = "Narrow";
+            }
+            VisualStateManager.GoToState(this, state, true);
+        }
+    }
+}
