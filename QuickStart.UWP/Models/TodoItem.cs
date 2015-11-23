8571433b5db7c82f8336ce9cae9480992966da39
Adrian Hall <adrianha@microsoft.com>
2015-11-23 12:38:01 -0800

    Renamed TaskItem to TodoItem to match quickstart server side

---
+++ QuickStart.UWP/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace QuickStart.UWP.Models
+{
+    class TodoItem
+    {
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Title { get; set; }
+
+        [JsonProperty("complete")]
+        public bool Completed { get; set; }
+    }
+}
