b153243f7301665972c9e6c8613b5bd13445e47f
Adrian Hall <adrianha@microsoft.com>
2015-11-24 11:40:07 -0800

    Updated CSS for Cordova client

--- Quickstart.Cordova/Gulpfile.js
+++ Quickstart.Cordova/Gulpfile.js
@@ -1,17 +1,23 @@
 var babelify = require('babelify'),
     browserify = require('browserify'),
     gulp = require('gulp'),
+    npmfiles = require('gulp-npm-files'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     source = require('vinyl-source-stream');
 
 gulp.task('default', [ 'build' ]);
-gulp.task('build', [ 'js:bundle', 'css:bundle' ]);
+gulp.task('build', [ 'js:bundle', 'css:bundle', 'libs:copy' ]);
 
 gulp.task('css:bundle', function () {
     gulp.src('./src/scss/index.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('libs:copy', function () {
+    gulp.src(npmfiles(), { base: './' })
+        .pipe(gulp.dest('./www/lib'));
 });
 
 gulp.task('js:bundle', function () {
--- Quickstart.Cordova/src/js/index.js
+++ Quickstart.Cordova/src/js/index.js
@@ -25,47 +25,54 @@ app.on('deviceready', function () {
     var el = {
         todoitems: document.querySelector('#todo-items'),
         summary: document.querySelector('#summary'),
-        refreshlist: document.querySelector('#refresh-tasks'),
+        refreshicon: document.querySelector('#refresh-icon'),
         addnewtask: document.querySelector('#add-new-task'),
         newitemtextbox: document.querySelector('#new-item-text')
     };
     
+    function setRefreshIcon(loading) {
+        if (loading) {
+            el.refreshicon.className = 'fa fa-refresh fa-spin';
+        } else {
+            el.refreshicon.className = 'fa fa-refresh';
+        }
+
+    }
+    
     // This is called whenever we want to refresh the contents from the database
     function refreshTaskList() {
-      el.summary.innerHTML = 'Loading...';
-      console.log('taskStore = ', taskStore);
+        setRefreshIcon(true);
 
-      taskStore.read({ complete: false }).then((todoItems) => {
-          console.log('Read the taskStore: items = ', todoItems);
-          let count = 0;
-          el.todoitems.innerHTML = ''; 
-          todoItems.forEach((entry, index) => {
-              let checked = entry.complete ? ' checked="checked"': '';
-              let entrytext = entry.text.replace('"', '&quot;');
-              let html = `<li data-todoitem-id="${entry.id}"><input type="checkbox" class="item-complete"${checked}><div><input class="item-text" value="${entrytext}"></div></li>`;
-              el.todoitems.innerHTML += html;
-              count++;
-          });  
-          el.summary.innerHTML = `<strong>${count}</strong> item(s)`;  
-      }).catch((error) => {
-          console.error('Error reading task store: ', error);
-          el.summary.innerHTML = '<strong>Error reading store.</strong>';
-      });
+        taskStore.read({ complete: false }).then((todoItems) => {
+            let count = 0;
+            el.todoitems.innerHTML = ''; 
+            todoItems.forEach((entry, index) => {
+                let checked = entry.complete ? ' checked="checked"': '';
+                let entrytext = entry.text.replace('"', '&quot;');
+                let html = `<li data-todoitem-id="${entry.id}"><input type="checkbox" class="item-complete"${checked}><div><input class="item-text" value="${entrytext}"></div></li>`;
+                el.todoitems.innerHTML += html;
+                count++;
+            });  
+            el.summary.innerHTML = `<strong>${count}</strong> item(s)`;  
+            setRefreshIcon(false);
+        }).catch((error) => {
+            console.error('Error reading task store: ', error);
+            el.summary.innerHTML = '<strong>Error reading store.</strong>';
+            setRefreshIcon(false);
+        });
     }
     
     // Set up the event handler for clicking on Refresh Tasks
-    el.refreshlist.addEventListener('click', refreshTaskList);
+    el.refreshicon.addEventListener('click', refreshTaskList);
     refreshTaskList();
     el.newitemtextbox.focus();
     
     // Set up the event handler for clicking on Add New Task
     el.addnewtask.addEventListener('click', function (event) {
-        console.log('Adding new item to the store');
         let itemText = el.newitemtextbox.value;
         if (itemText !== '') {
             taskStore.insert({ text: itemText, complete: false }).then(refreshTaskList);
         }
-        console.log('Resetting new item text box');
         el.newitemtextbox.value = '';
         el.newitemtextbox.focus();
         event.preventDefault();
@@ -73,8 +80,6 @@ app.on('deviceready', function () {
     
     // Set up the event handler for updating a task
     el.todoitems.addEventListener('change', function (event) {
-        console.log('New Change Event Listener: ', event);
-        
         let target = event.target;
         // Only handle change events on the item-text or item-complete elements
         if (target.className === 'item-text' || target.className === 'item-complete') {
@@ -83,28 +88,22 @@ app.on('deviceready', function () {
             if (!li) {
                 console.log('Could not find LI - break');
                 return;
-            }
-            console.log('LI Element = ', li);
-            
+            }            
             // The ID is the data-todoitem-id attribute value
             let id = li.getAttribute('data-todoitem-id');
-            console.log('The ID to be modified = ', id);
             
             // Pull the original record
             taskStore.getById(id).then((element) => {
-                console.log('Element Data = ', element);
                 // Replace the complete or text field from the event
                 if (target.className === 'item-text') {
                     element.text = target.value.trim();
                 } else if (target.className === 'item-complete') {
                     element.complete = target.checked;
-                }
-                
+                }                
                 // Update the record with the new information
                 return taskStore.update(element);
             })
             .then((element) => {
-                console.log('Updated element successfully');
                 refreshTaskList();
             });
             event.preventDefault();
